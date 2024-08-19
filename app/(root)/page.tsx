4b5a26b57ab55fbf68a09cc2c58ae024128bd4b8
RoronoaZoroDAce <90967650+RoronoaZoroDAce@users.noreply.github.com>
2024-08-19 16:42:41 +0530

    Implement layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Aaditya",
+    lastName: "Malap",
+    email: "aadityamalap666@gmail.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        Recent transactions
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 1234.50}, {currentBalance: 504.50}]} />
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/MobileNav.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+
+const MobileNav = ({ user }): MobileNavProps => {
+  const pathname = usePathname();
+
+  return (
+    <section className="w-full ,ax-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className="mb-12 cursor-pointer items-center flex gap-2"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="nexGen logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              NexGen
+            </h1>
+          </Link>
+
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathname === item.route ||
+                    pathname.startsWith("${item.route}/");
+
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bankGradient": isActive,
+                        })}
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                          className={cn({
+                            "brightness-0-[3] invert-0": isActive,
+                          })}
+                        />
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "text-white": isActive,
+                          })}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+                USER
+              </nav>
+            </SheetClose>
+            FOOTER
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
